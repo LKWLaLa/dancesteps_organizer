08b1d585081f1d4dac18abff5ed0f15ca6eec651
LKWLaLa <lkwlala@gmail.com>
2016-03-25 17:54:38 -0400

    begin methods for users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,48 @@
 class UsersController < ApplicationController
+
+  get '/signup' do
+    erb :'users/new'
+  end
+
+  get '/login' do
+    erb :'users/login'
+  end
+
+  get '/users/:id' do
+    erb :'users/show'
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
+
+
+  post '/signup' do
+    @user = User.new(params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to '/users/:id'
+    else
+      error message and redirect to '/signup'
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/users/:id'
+    else
+      redirect '/login'
+    end
+  end
+
+  
+
+
+
 end
