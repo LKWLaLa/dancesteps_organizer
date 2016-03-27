b0a1dc774b1f6331349410fb1ab9b223331a076a
LKWLaLa <lkwlala@gmail.com>
2016-03-26 23:24:04 -0400

    add seeds, new and edit step views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,16 @@
 # Add seed data here. Seed your database with `rake db:seed`
 
+Style.create(name: "Lindy Hop")
+Style.create(name: "Balboa")
+Style.create(name: "Solo Jazz")
+Style.create(name: "Blues")
+Style.create(name: "Modern")
+Style.create(name: "Ballroom")
+Style.create(name: "Tap")
+Style.create(name: "Hip Hop")
+Style.create(name: "Shag")
+Style.create(name: "Charleston")
+
+
+
+
